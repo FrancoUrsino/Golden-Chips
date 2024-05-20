a34eda56c8e354ceeec3098722ec2518cd82a9a2
Franco Ursino <franncoursinno@gmail.com>
2024-05-20 10:49:08 -0300

    primera pre entrega JS

--- js/script.js
+++ js/script.js
@@ -5,24 +5,26 @@ document.write(`<p>Bienvenido, ${username}!</p>`);
 
 // Paso 2: Simulador Financiero
 // Solicitar datos financieros mediante prompts
-let income = parseFloat(prompt('escribe tu ingreso mensual:'));
+let income = parseFloat(prompt('Escribe tu ingreso mensual:'));
 while (isNaN(income)) {
-  income = parseFloat(prompt('Escribiste un valor no válido. Escribe tu ingreso mensual:'));
+  income = parseFloat(prompt('Escribiste un valor incorrecto. Escribe tu ingreso mensual:'));
 }
 
 let expenses = parseFloat(prompt('Escribe el total de tus gastos mensuales:'));
 while (isNaN(expenses)) {
-  expenses = parseFloat(prompt('Escribiste un valor no válido. Escribe tus gastos mensuales:'));
+  expenses = parseFloat(prompt('Escribiste un valor incorrecto. Escribe tus gastos mensuales:'));
 }
 
 let savings = parseFloat(prompt('Escribe tus ahorros actuales, si no tenes colocá 0:'));
 while (isNaN(savings)) {
-  savings = parseFloat(prompt('Escribiste un valor no válido. Escribe tus ahorros actuales:'));
+  savings = parseFloat(prompt('Escribiste un valor incorrecto. Escribe tus ahorros actuales:'));
 }
 
-let hasInvestments = prompt('¿Tienes inversiones actuales? Escribe 1 para Sí o 2 para No:');
+let hasInvestments = prompt(`¿Tienes inversiones actualmente? Escribe:
+1= Sí
+2= No`);
 while (hasInvestments !== '1' && hasInvestments !== '2') {
-  hasInvestments = prompt('Escribiste una opción inválida. ¿Tienes inversiones actuales? escribe 1 para Sí o 2 para No:');
+  hasInvestments = prompt('Escribiste una opción inválida. ¿Tienes inversiones actualmente? escribe 1 para Sí o 2 para No:');
 }
 
 let investments = 0;
@@ -35,11 +37,11 @@ if (hasInvestments === '1') {
 
 // Mostrar los datos ingresados
 document.write(`<h2>${username} debajo se verán los datos ingresados</h2>`);
-document.write('<p>Ingresos mensuales: $' + income.toFixed(2) + '</p>');
-document.write('<p>Gastos mensuales: $' + expenses.toFixed(2) + '</p>');
-document.write('<p>Ahorros actuales: $' + savings.toFixed(2) + '</p>');
+document.write(`<p>Ingresos mensuales: $${income.toFixed(2)}</p>`);
+document.write(`<p>Gastos mensuales: $${expenses.toFixed(2)}</p>`);
+document.write(`<p>Ahorros actuales: $${savings.toFixed(2)}</p>`);
 if (hasInvestments === '1') {
-  document.write('<p>Inversiones actuales: $' + investments.toFixed(2) + '</p>');
+  document.write(`<p>Inversiones actuales: $${investments.toFixed(2)}</p>`);
 } else {
   document.write('<p>No tienes inversiones actuales.</p>');
 }
@@ -54,12 +56,12 @@ document.getElementById('calculate-button').addEventListener('click', function()
   
   // Mostrar resultados
   document.write(`<h2>${username} ¡Estos son tus resultados!</h2>`);
-  document.write('<p>Ingresos mensuales: $' + income.toFixed(2) + '</p>');
-  document.write('<p>Gastos mensuales: $' + expenses.toFixed(2) + '</p>');
-  document.write('<p>Ahorros actuales: $' + savings.toFixed(2) + '</p>');
+  document.write(`<p>Ingresos mensuales: $${income.toFixed(2)}</p>`);
+  document.write(`<p>Gastos mensuales: $${expenses.toFixed(2)}</p>`);
+  document.write(`<p>Ahorros actuales: $${savings.toFixed(2)}</p>`);
   if (hasInvestments === '1') {
-    document.write('<p>Inversiones actuales: $' + investments.toFixed(2) + '</p>');
+    document.write(`<p>Inversiones actuales: $${investments.toFixed(2)}</p>`);
   }
-  document.write('<p>Balance Mensual: $' + balance.toFixed(2) + '</p>');
-  document.write('<p>Total de Activos: $' + totalAssets.toFixed(2) + '</p>');
+  document.write(`<p>Balance Mensual: $${balance.toFixed(2)}</p>`);
+  document.write(`<p>Activos: $${totalAssets.toFixed(2)}</p>`);
 });
